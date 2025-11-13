26394e8dbdfe89c64504b2d01ce93b2d7ce6c607
Peter Seggi <pedro.seggiaro@gmail.com>
2025-11-13 01:20:01 -0300

    query 2 refactor

--- src/queries/query2.js
+++ src/queries/query2.js
@@ -20,62 +20,83 @@ export const query2 = async (redisClient, mongoClient) => {
   // Query MongoDB
   const pipeline = [
     {
-        $match: {
-          estado: { $in: ['Abierto', 'En proceso'] }
-        }
-      },
-      {
-        $lookup: {
-          from: 'clientes',
-          let: { poliza_num: '$nro_poliza' },
-          pipeline: [
-            {
-              $match: {
-                'polizas.nro_poliza': { $exists: true },
-                $expr: {
-                  $in: ['$$poliza_num', '$polizas.nro_poliza']
-                }
-              }
-            },
-            {
-              $project: {
-                _id: 0,
-                id_cliente: 1,
-                nombre: 1,
-                apellido: 1,
-                dni: 1,
-                email: 1,
-                telefono: 1,
-                ciudad: 1,
-                provincia: 1
+      $match: {
+        estado: { $in: ['Abierto'] }
+      }
+    },
+    {
+      $lookup: {
+        from: 'clientes',
+        let: { polizaNum: '$nro_poliza' },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $in: ['$$polizaNum', '$polizas.nro_poliza']
               }
             }
-          ],
-          as: 'cliente'
-        }
-      },
-      {
-        $unwind: {
-          path: '$cliente',
-          preserveNullAndEmptyArrays: true
-        }
-      },
-      {
-        $project: {
-          _id: 0,
-          id_siniestro: 1,
-          nro_poliza: 1,
-          fecha: 1,
-          tipo: 1,
-          monto_estimado: 1,
-          descripcion: 1,
-          estado: 1,
-          cliente: 1
-        }
-      },
-      {
-        $sort: { fecha: -1 }
+          },
+          {
+            $project: {
+              _id: 0,
+              id_cliente: 1,
+              nombre: 1,
+              apellido: 1,
+              dni: 1,
+              email: 1,
+              telefono: 1,
+              ciudad: 1,
+              provincia: 1
+            }
+          }
+        ],
+        as: 'cliente'
       }
+    },
+    {
+      $unwind: {
+        path: '$cliente',
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $addFields: {
+        fecha_date: {
+          $let: {
+            vars: { parts: { $split: ['$fecha', '/'] } },
+            in: {
+              $dateFromParts: {
+                year: { $toInt: { $arrayElemAt: ['$$parts', 2] } },
+                month: { $toInt: { $arrayElemAt: ['$$parts', 1] } },
+                day: { $toInt: { $arrayElemAt: ['$$parts', 0] } }
+              }
+            }
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        id_siniestro: 1,
+        nro_poliza: 1,
+        fecha: 1,
+        tipo: 1,
+        monto_estimado: 1,
+        descripcion: 1,
+        estado: 1,
+        cliente: 1,
+        fecha_date: 1
+      }
+    },
+    {
+      $sort: { fecha_date: -1 }
+    },
+    {
+      $project: {
+        fecha_date: 0
+      }
+    }
   ];
   
   const results = await db.collection('siniestros').aggregate(pipeline).toArray();
